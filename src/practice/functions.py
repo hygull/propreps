65f7ff721e441d19bd2e80e1addda2b7b9058026
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2024-12-31 22:47:51 +0530

    Update normal python script named functions.py with 2 more functions defined inside it

--- src/practice/functions.py
+++ src/practice/functions.py
@@ -9,4 +9,15 @@ def subtraction(a, b):
     return a - b
 
 
-print(addition(60, 40) + subtraction(150, 50))
+def multiplication(a, b):
+    return a * b
+
+
+def division(a, b):
+    # Assumes that b is always a non zero number
+    return a / b
+
+
+print(
+    addition(60, 40) + subtraction(150, 50) + multiplication(50, 2) + division(1500, 15)
+)
