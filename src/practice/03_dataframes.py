d6536132037aa310a3407616596b14c92a503b00
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2025-01-01 00:12:49 +0530

    Update example based on pandas; also update output

--- src/practice/03_dataframes.py
+++ src/practice/03_dataframes.py
@@ -2,16 +2,17 @@ import pandas as pd
 
 
 df = pd.DataFrame(
-    {"first_name": ["Righikesh", "Hemkesh", "Malinikesh"], "age": [32, 30, 28]}
+    {"first_name": ["Rishikesh", "Hemkesh", "Malinikesh"], "age": [32, 30, 28]}
 )
+df["last_name"] = "Agrawani"
 
 print(df)
 """
 OUTPUT (python3 src/practice/03_dataframes.py)
 ==============================================
 
-   first_name  age
-0   Righikesh   32
-1     Hemkesh   30
-2  Malinikesh   28
+   first_name  age last_name
+0   Rishikesh   32  Agrawani
+1     Hemkesh   30  Agrawani
+2  Malinikesh   28  Agrawani
 """
