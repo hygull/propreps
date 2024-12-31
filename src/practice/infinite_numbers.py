67c218f801e76bf67fb30e8606d222f8da06c5f7
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2024-12-31 22:59:41 +0530

    Update normal python script named infinite_numbers.py with 1 more way to represent infinite numbers; also add 1 example to check if any number is infinte or not

--- src/practice/infinite_numbers.py
+++ src/practice/infinite_numbers.py
@@ -1,5 +1,5 @@
 import math
-
+import numpy as np
 
 # 1st way
 n1 = float("inf")
@@ -19,3 +19,20 @@ print("n1 == n3", n1 == n3)
 print("n2 == n4", n2 == n4)
 print("n1 == n2", n1 == n2)
 print("n3 == n4", n3 == n4)
+
+
+# 3rd way
+n5 = np.inf
+n6 = -np.inf
+
+print("n5", n5)
+print("n6", n6)
+
+print("n1 == n5", n1 == n5)
+print("n2 == n6", n2 == n6)
+
+
+# Check if number is infinite
+print(math.isinf(n1))  # True
+print(math.isinf(n2))  # True
+print(math.isinf(10))  # False
