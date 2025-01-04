3ac92c2e914efc1f6300cbb86ecc8eb62005f647
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2025-01-04 23:51:44 +0530

    Add more examples related to left, right, center alignment of strings withing available space

--- src/practice/07_string_formatting_using_format_and_f_strings_py3.6.py
+++ src/practice/07_string_formatting_using_format_and_f_strings_py3.6.py
@@ -10,3 +10,23 @@ O/P
 ---
 My name is Rishikesh Agrawani and I am 32 years old. I like math constant PI i.e. 3.14.
 """
+
+# Placeholder -> {}, Modifier -> , (Use a comma as a thousand separator)
+print(f"Dwaparyuga is {5000:,} years old.")
+
+# Left aligns the result (within the available space)
+print(f"Sometimes we more think about {100:<10} different ways to solve a problem.")
+
+# Right aligns the result (within the available space)
+print(f"Sometimes we more think about {100:>10} different ways to solve a problem.")
+
+# Center aligns the result (within the available space)
+print(f"Sometimes we more think about {100:^10} different ways to solve a problem.")
+"""
+O/P
+---
+Dwaparyuga is 5,000 years old.
+Sometimes we more think about 100        different ways to solve a problem.
+Sometimes we more think about        100 different ways to solve a problem.
+Sometimes we more think about    100     different ways to solve a problem.
+"""
