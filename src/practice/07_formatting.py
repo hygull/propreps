4f8f6154e6698e7af02daeb523499b2b2fbfe339
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2025-01-04 23:34:11 +0530

    Add a script with simple string formatting example

---
+++ src/practice/07_formatting.py
@@ -0,0 +1,12 @@
+fullname = "Rishikesh Agrawani"
+age = 32
+pi = 3.142
+
+print(
+    f"My name is {fullname} and I am {age} years old. I like math constant PI i.e. {pi:.2f}."
+)
+"""
+O/P
+---
+My name is Rishikesh Agrawani and I am 32 years old. I like math constant PI i.e. 3.14.
+"""
