6e3b08406845b640d80a69de22e35e9a3fb40073
Rishikesh Agrawani <rishikesh0014051992@gmail.com>
2025-03-02 14:12:22 +0530

    Add example related to the usage of for loop in bash scripting

---
+++ src/shell_scripts/for_loop_examples.sh
@@ -0,0 +1,20 @@
+sentence="This Sunday looks better because it is full of energy and happiness"
+
+for word in $sentence
+do
+	echo "Word -> $word"
+done
+
+# ip-192-168-1-107:shell_scripts hygull$ sh for_loop_examples.sh 
+# Word -> This
+# Word -> Sunday
+# Word -> looks
+# Word -> better
+# Word -> because
+# Word -> it
+# Word -> is
+# Word -> full
+# Word -> of
+# Word -> energy
+# Word -> and
+# Word -> happiness
